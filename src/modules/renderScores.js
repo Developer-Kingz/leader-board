2c92f5e959e4c726f386d215d6a6b356577d28dc
Kingsley Igbor <chidoziek2@gmail.com>
2023-06-30 11:35:26 +0100

    used async and await to get scores

--- src/modules/renderScores.js
+++ src/modules/renderScores.js
@@ -1,17 +1,26 @@
-const renderScores = () => {
+const renderScores = async () => {
   const leaderboard = document.querySelector('.name ul');
   leaderboard.innerHTML = '';
 
-  fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ny53bvmTZNAqKKu9OSx3/scores/',
-  )
-    .then((response) => response.json())
-    .then((json) => {
-      json.result.forEach((score) => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${score.user}: ${score.score}`;
-        leaderboard.appendChild(listItem);
-      });
+  try {
+    const response = await fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ny53bvmTZNAqKKu9OSx3/scores/',
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch scores: ${response.status}`);
+    }
+
+    const json = await response.json();
+
+    json.result.forEach((score) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${score.user}: ${score.score}`;
+      leaderboard.appendChild(listItem);
     });
+  } catch (error) {
+    throw new Error('Unable to post');
+  }
 };
+
 export default renderScores;
